fed368187cdd8fd7b3aa67bda2da56561e1116fc
Gurkan UCAR <ucar.gurkan@hotmail.com>
2026-02-24 23:13:30 +0300

    feat: add transparent mode toggle

--- app.go
+++ app.go
@@ -2,26 +2,90 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"syscall"
+	"time"
+	"unsafe"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
 
-// App struct
+var (
+	user32                = syscall.NewLazyDLL("user32.dll")
+	procFindWindowW       = user32.NewProc("FindWindowW")
+	procSetWindowLongPtrW = user32.NewProc("SetWindowLongPtrW")
+	procGetWindowLongPtrW = user32.NewProc("GetWindowLongPtrW")
+	procGetAsyncKeyState  = user32.NewProc("GetAsyncKeyState")
+)
+
+const (
+	GWL_EXSTYLE       = ^uintptr(19) // -20 as uintptr (two's complement)
+	WS_EX_TRANSPARENT = uintptr(0x00000020)
+	WS_EX_LAYERED     = uintptr(0x00080000)
+	VK_ESCAPE         = uintptr(0x1B)
+)
+
 type App struct {
-	ctx context.Context
+	ctx          context.Context
+	hwnd         uintptr
+	clickThrough bool
 }
 
-// NewApp creates a new App application struct
 func NewApp() *App {
 	return &App{}
 }
 
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+	go a.listenGlobalHotkeys()
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func (a *App) getWindowHandle() uintptr {
+	if a.hwnd != 0 {
+		return a.hwnd
+	}
+
+	titlePtr, err := syscall.UTF16PtrFromString("EpicPenGo")
+	if err != nil {
+		return 0
+	}
+
+	ret, _, _ := procFindWindowW.Call(
+		0,
+		uintptr(unsafe.Pointer(titlePtr)),
+	)
+	a.hwnd = ret
+	return a.hwnd
+}
+
+func (a *App) toggleClickThrough() {
+	hwnd := a.getWindowHandle()
+	if hwnd == 0 {
+		return
+	}
+
+	style, _, _ := procGetWindowLongPtrW.Call(hwnd, GWL_EXSTYLE)
+
+	a.clickThrough = !a.clickThrough
+
+	if a.clickThrough {
+		style |= WS_EX_LAYERED | WS_EX_TRANSPARENT
+	} else {
+		style &^= WS_EX_TRANSPARENT
+		style |= WS_EX_LAYERED
+	}
+
+	procSetWindowLongPtrW.Call(hwnd, GWL_EXSTYLE, style)
+
+	runtime.EventsEmit(a.ctx, "modeChanged", a.clickThrough)
+}
+
+func (a *App) listenGlobalHotkeys() {
+	for {
+		ret, _, _ := procGetAsyncKeyState.Call(VK_ESCAPE)
+		if ret&0x8000 != 0 {
+			a.toggleClickThrough()
+			time.Sleep(300 * time.Millisecond)
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
 }
